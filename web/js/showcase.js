21ddda0c13420b7a33bf49055d82dd37f2cf8482
Trishkin Vadim <halumein@gmail.com>
2016-11-25 16:18:26 +0500

    fixes

--- web/js/showcase.js
+++ web/js/showcase.js
@@ -8,26 +8,26 @@ halumein.showcase = {
 
         $quickSearchInput = $('[data-role=quick-search]');
         $searchBlock = $('[data-role=search-block]');
-        $productsForSearch = $searchBlock.find('[data-role=showcase-product]');
 
-		$showcaseCategoryButton= $('[data-role=showcase-category-button]');
-        $showcaseCategory= $('[data-role=showcase-category]');
-		$showcaseProduct= $('[data-role=showcase-product]');
+		$showcaseCategoryButton = $('[data-role=showcase-category-button]');
+		$showcaseProduct = $('[data-role=showcase-product]');
         $breadcrumbs = $('[data-role=breadcrumbs]');
         $amountInput = $('[data-role=showcase-item-amount-input]');
         breadcrumbsButton = '[data-role=breadcrumbs-button]';
+
+        $showcaseItems = $('.showcase-item');
+
 
         /* для дебага */
         $showAllProductsButton = $('[data-role=show-all]');
 
 		$showcaseCategoryButton.on('click', function() {
-
             var self = this,
                 categoryId = $(self).data('category-id'),
                 title = $(self).find('.showcase-item-title').text();
 
-			halumein.showcase.hideCurrentSection();
-            halumein.showcase.showSection(categoryId);
+            console.log(categoryId);
+            halumein.showcase.renderTargetContent(categoryId);
             halumein.showcase.addBreadcrumb(title, categoryId);
 		});
 
@@ -35,21 +35,19 @@ halumein.showcase = {
             $(this).select();
         });
 
-
         $amountInput.keydown(function(e) {
             var code = e.which,
                 self = this;
             if(code==13) {
                 $thisProductBlock = $(self).closest('[data-role=showcase-product]');
                 halumein.showcase.addToCart($thisProductBlock);
+                $(self).blur();
             };
 
         });
 
         $showAllProductsButton.on('click', function() {
             halumein.showcase.showAllProducts();
-            halumein.showcase.hideCurrentSection();
-            halumein.showcase.showSection('search');
             halumein.showcase.addBreadcrumb('Все товары', 'search-block');
 
         });
@@ -68,14 +66,7 @@ halumein.showcase = {
         // быстрый поиск
         $quickSearchInput.keyup(function(e) {
             var self = this;
-            var $currentSection = $('.current-active');
             if($(self).val().length >= 3) {
-                if ($currentSection.data('role') !== 'search-block') {
-                    halumein.showcase.hideCurrentSection();
-                    halumein.showcase.showSection('search');
-                    halumein.showcase.addBreadcrumb('Результаты поиска', 'search');
-                }
-
                 delay(function() {
                     halumein.showcase.searchByName($(self).val());
                 }, 350);
@@ -94,8 +85,7 @@ halumein.showcase = {
             var self = this,
                 target = $(self).data('target');
 
-            halumein.showcase.hideCurrentSection();
-            halumein.showcase.showSection(target);
+            halumein.showcase.renderTargetContent(target);
             $quickSearchInput.val('');
             $(self).nextAll().remove();
         });
@@ -119,6 +109,16 @@ halumein.showcase = {
 
 	},
 
+    renderTargetContent : function(categoryId) {
+        $.each($showcaseItems, function(index, itemBlock) {
+            if ($(itemBlock).data('parent-id') === categoryId) {
+                $(itemBlock).removeClass('hidden');
+            } else {
+                $(itemBlock).addClass('hidden');
+            }
+        });
+    },
+
     addToCart : function($productBlock) {
 
         var self = $productBlock,
@@ -138,22 +138,16 @@ halumein.showcase = {
 
         $(self).find('[data-role=showcase-item-amount-input]').val(1);
     },
-
-	hideCurrentSection : function() {
-        $(".current-active").addClass('hidden').removeClass('current-active');
-	},
-
-    showSection : function(categoryId) {
-        $('[data-category-case-id='+ categoryId +']').removeClass('hidden').addClass('current-active');
-    },
-
     addBreadcrumb : function(title, target) {
         $breadcrumbs.append('<li class="showcase-breadcrumbs" data-role="breadcrumbs-button" data-target="' + target + '">' + title + '</li>');
     },
 
     searchByName : function(queryString) {
+        $.each($showcaseCategoryButton, function(index, block) {
+            $(block).addClass('hidden');
+        });
         if (queryString != '') {
-            $.each($productsForSearch, function(index, productBlock) {
+            $.each($showcaseProduct, function(index, productBlock) {
                 if ($(productBlock).data('product-name').toLowerCase().indexOf(queryString.toLowerCase()) >=0) {
                     $(productBlock).removeClass('hidden');
                 } else {
@@ -163,7 +157,11 @@ halumein.showcase = {
         }
     },
     showAllProducts : function() {
-        $.each($productsForSearch, function(index, productBlock) {
+        $.each($showcaseCategoryButton, function(index, categoryBlock) {
+            $(categoryBlock).addClass('hidden');
+        });
+
+        $.each($showcaseProduct, function(index, productBlock) {
             $(productBlock).removeClass('hidden');
         });
     },
--- widgets/views/showCase.php
+++ widgets/views/showCase.php
@@ -11,7 +11,7 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <input class="form-control" type="text" name="name" value="" placeholder="быстрый поиск товара" data-role="quick-search">
+        <input class="form-control" type="text" name="name" value="" placeholder="быстрый поиск товара" data-role="quick-search" autocomplete="off">
     </div>
 </div>
 
@@ -28,7 +28,7 @@
         <div class="showcase-main current-active" data-category-case-id="main">
             <?php foreach ($categories as $categoryId => $category) { ?>
                 <?php if (is_null($category['parentCategoryId'])) { ?>
-                <div class="showcase-item category-item" data-role="showcase-category-button" data-category-id="<?= $categoryId ?>">
+                <div class="showcase-item category-item" data-role="showcase-category-button" data-category-id="<?= $categoryId ?>" data-parent-id="main" >
                     <div class="showcase-item-title text-center">
                         <?= $category['name'] ?>
                     </div>
@@ -38,85 +38,49 @@
                 </div>
                 <?php } ?>
             <?php } ?>
-        </div>
 
-        <?php foreach ($categories as $categoryId => $category) { ?>
-                <div class="showcase-category hidden" data-role="showcase-category" data-category-case-id="<?= $categoryId ?>">
-                    <?php foreach ($category['childCategories'] as $key => $childCategory) { ?>
-                        <div class="showcase-item category-item" data-role="showcase-category-button" data-category-id="<?= $childCategory ?>" >
-                            <div class="showcase-item-title text-center">
-                                <?= $categories[$childCategory]['name'] ?>
-                            </div>
-                            <div class="showcase-item-image text-center">
-                                <img src="<?=  $categories[$childCategory]['image'] ?>" alt="<?= $category['name'] ?>">
-                            </div>
-                        </div>
-                    <?php } ?>
-
-                    <?php foreach ($category['products'] as $productId => $product) { ?>
-                        <div class="showcase-item product-item" data-product-id="<?= $productId ?>" data-role="showcase-product" >
-                            <div class="showcase-item-title text-center">
-                                <?= $product['name'] ?>
-                            </div>
-                            <div class="showcase-item-image text-center">
-                                <img src="<?= $product['image'] ?>" alt="<?= $product['name'] ?>">
-                            </div>
-                            <div class="showcase-item-price">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <input class="showcase-item-amount-input text-right" type="number" name="" value="1" data-role="showcase-item-amount-input">
-                                    </div>
-                                    <div class="col-sm-6 text-right" data-role="showcase-item-price" data-default-price="<?= $product['price'] ?>">
-                                        <?= $product['price'] ?> руб.
-                                    </div>
+            <?php foreach ($categories as $categoryId => $category) { ?>
+                        <?php foreach ($category['childCategories'] as $key => $childCategory) { ?>
+                            <div class="showcase-item category-item hidden" data-role="showcase-category-button" data-category-id="<?= $childCategory ?>" data-parent-id=<?=$categoryId?>>
+                                <div class="showcase-item-title text-center">
+                                    <?= $categories[$childCategory]['name'] ?>
                                 </div>
-                                <!-- <input class="showcase-item-price-input text-right" type="text" name="" value="<?php // $product['price'] ?>" data-base-price="<?php // $product['price'] ?>"> -->
+                                <div class="showcase-item-image text-center">
+                                    <img src="<?=  $categories[$childCategory]['image'] ?>" alt="<?= $category['name'] ?>">
+                                </div>
                             </div>
-                        </div>
-                        <?= pistol88\cart\widgets\BuyButton::widget([
-                            'model' => $product['model'],
-                            'text' => '<i class="glyphicon glyphicon-shopping-cart"></i>',
-                            'htmlTag' => 'div',
-                            'cssClass' => 'btn btn-default hidden'
-                        ]) ?>
-                    <?php } ?>
+                        <?php } ?>
 
-                </div>
-        <?php } ?>
-<?php /*
-    <div class="showcase-search hidden" data-category-case-id="search" data-role="search-block">
-        <?php foreach ($categories as $categoryId => $category) { ?>
-            <?php foreach ($category['products'] as $productId => $product) { ?>
-                <div class="showcase-item product-item hidden" data-product-id="<?= $productId ?>" data-role="showcase-product" data-product-name="<?= $product['name'] ?>">
-                    <div class="showcase-item-title text-center">
-                        <?= $product['name'] ?>
-                    </div>
-                    <div class="showcase-item-image text-center" data-role="image-container" data-img-src="<?= $product['image'] ?>">
-                        <img src="" alt="<?= $product['name'] ?>">
-                    </div>
-                    <div class="showcase-item-price">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <input class="showcase-item-amount-input text-right" type="number" name="" value="1" data-role="showcase-item-amount-input">
+                        <?php foreach ($category['products'] as $productId => $product) { ?>
+                            <div class="showcase-item product-item hidden" data-product-id="<?= $productId ?>" data-role="showcase-product" data-parent-id=<?=$categoryId?> data-product-name="<?= $product['name'] ?>">
+                                <div class="showcase-item-title text-center">
+                                    <?= $product['name'] ?>
+                                </div>
+                                <div class="showcase-item-image text-center">
+                                    <img src="<?= $product['image'] ?>" alt="<?= $product['name'] ?>">
+                                </div>
+                                <div class="showcase-item-price">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <input class="showcase-item-amount-input text-right" type="number" name="" value="1" data-role="showcase-item-amount-input">
+                                        </div>
+                                        <div class="col-sm-6 text-right" data-role="showcase-item-price" data-default-price="<?= $product['price'] ?>">
+                                            <?= $product['price'] ?> руб.
+                                        </div>
+                                    </div>
+                                    <!-- <input class="showcase-item-price-input text-right" type="text" name="" value="<?php // $product['price'] ?>" data-base-price="<?php // $product['price'] ?>"> -->
+                                </div>
                             </div>
-                            <div class="col-sm-6 text-right" data-role="showcase-item-price" data-default-price="<?= $product['price'] ?>">
-                                <?= $product['price'] ?> руб.
-                            </div>
-                        </div>
-                        <!-- <input class="showcase-item-price-input text-right" type="text" name="" value="<?php // $product['price'] ?>" data-base-price="<?php // $product['price'] ?>"> -->
-                    </div>
-                </div>
-                <?= pistol88\cart\widgets\BuyButton::widget([
-                    'model' => $product['model'],
-                    'text' => '<i class="glyphicon glyphicon-shopping-cart"></i>',
-                    'htmlTag' => 'div',
-                    'cssClass' => 'btn btn-default hidden'
-                ]) ?>
+                            <?= pistol88\cart\widgets\BuyButton::widget([
+                                'model' => $product['model'],
+                                'text' => '<i class="glyphicon glyphicon-shopping-cart"></i>',
+                                'htmlTag' => 'div',
+                                'cssClass' => 'btn btn-default hidden'
+                            ]) ?>
+                        <?php } ?>
             <?php } ?>
-        <?php } ?>
-    </div>
 
-    */ ?>
+        </div>
 
     </div>
 </div>
