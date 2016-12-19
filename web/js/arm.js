1f4b432bfbac0bb354563c7527e8f173d84c8c56
Trishkin Vadim <halumein@gmail.com>
2016-12-19 14:37:58 +0500

    div scroll

--- web/js/arm.js
+++ web/js/arm.js
@@ -4,11 +4,11 @@ if (typeof halumein == "undefined" || !halumein) {
 
 halumein.arm = {
 	init : function() {
-        /*
+
         if($(document).width() > 1100) {
             $('.slide-block').css({'height': screen.height-240, 'width': '277px', 'overflow-x': 'hidden', 'position': 'fixed', 'overflow': 'hidden'});
             $('.slide-content').css({'height': screen.height-180, 'width': '288px', 'overflow-y': 'scroll'});
-            
+
             $(window).on('scroll', function(){
                 if($('body').scrollTop() > 210) {
                     $('.slide-block, .slide-content').css({'top': '50px', 'height': screen.height-220});
@@ -17,7 +17,7 @@ halumein.arm = {
                 }
             });
         }
-        */
+
     },
 }
 
